66baa85a6b0f25a17d17a0b49fae1cdcc08b4624
TIRUPATHI MADISHETTI <138445968+tirushetty@users.noreply.github.com>
2023-08-21 23:16:18 +0530

    Update script.sh
    error fixed

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "your jenkins intigration is ready to use"
-echo "================================"
-successfully triggered the git push into jenkins
+echo "========================================"
+successfully triggered the git push into jenkins 
